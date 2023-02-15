3b7a784e06ee95485facda3d07d4ee0139ca142c
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-02-15 13:43:39 -0500

    added pre-increment, pre-decrement, post-increment and post-decrement

--- Lab5BB/Rational.cpp
+++ Lab5BB/Rational.cpp
@@ -20,37 +20,38 @@ double Rational::getDenominator() const {
 }
 
 Rational Rational::operator+(const Rational& other) const {
-    double new_numerator = numerator * other.denominator + other.numerator * denominator;
-    double new_denominator = denominator * other.denominator;
-    Rational result(new_numerator, new_denominator);
+    double const newNumerator = numerator * other.denominator + other.numerator * denominator;
+    double const newDenominator = denominator * other.denominator;
+    Rational result(newNumerator, newDenominator);
     result.Reduce();
     return result;
 }
 
 Rational Rational::operator-(const Rational& other) const {
-    double new_numerator = numerator * other.denominator - other.numerator * denominator;
-    double new_denominator = denominator * other.denominator;
-    Rational result(new_numerator, new_denominator);
+    double const newNumerator = numerator * other.denominator - other.numerator * denominator;
+    double const newDenominator = denominator * other.denominator;
+    Rational result(newNumerator, newDenominator);
     result.Reduce();
     return result;
 }
 
 Rational Rational::operator*(const Rational& other) const {
-    double newNumerator = numerator * other.numerator;
-    double newDenominator = denominator * other.denominator;
+    double const newNumerator = numerator * other.numerator;
+    double const newDenominator = denominator * other.denominator;
     Rational newRational(newNumerator, newDenominator);
     newRational.Reduce();
     return newRational;
 }
 
 Rational Rational::operator/(const Rational& other) const {
-    double newNumerator = numerator * other.denominator;
-    double newDenominator = denominator * other.numerator;
+    double const newNumerator = numerator * other.denominator;
+    double const newDenominator = denominator * other.numerator;
     Rational newRational(newNumerator, newDenominator);
     newRational.Reduce();
     return newRational;
 }
 
+// Overloaded stream operators
 std::istream& operator>>(std::istream& in, Rational& r) {
     double num, denom;
     in >> num >> denom;
@@ -60,21 +61,11 @@ std::istream& operator>>(std::istream& in, Rational& r) {
 
 std::ostream& operator<<(std::ostream& out, const Rational& r) {
     out << r.numerator << "/" << r.denominator;
-    /*out << r.numerator << "\n";
-    out << "--" << "\n";
-    out << r.denominator;*/
     return out;
 }
 
-double Rational::getHighestCommonFactor(double a, double b) const {
-    // Euclidean algorithm to find the getHighestCommonFactor of a and b
-    while (b != 0) {
-        double temp = b;
-        b = fmod(a, b);
-        a = temp;
-    }
-    return a;
-}
+
+// Overloaded relational and equality operators
 
 bool Rational::operator<(const Rational& other) const {
     return this->getCurrentValue() < other.getCurrentValue();
@@ -103,4 +94,66 @@ bool Rational::operator==(const Rational& other) const {
 double Rational::getCurrentValue() const
 {
     return this->getNumerator() / this->getDenominator();
+}
+
+// Overloaded increment and decrement operators
+
+Rational& Rational::operator++() {
+    // pre-increment operator
+    if (denominator == 1) {
+        numerator++;
+        return *this;
+    }
+    else {
+        // throw an exception if the denominator is not 1
+        throw std::invalid_argument("Denominator must be 1 to use pre-increment operator.");
+    }
+}
+
+Rational& Rational::operator--() {
+    // pre-decrement operator
+    if (denominator == 1) {
+        numerator--;
+        return *this;
+    }
+    else {
+        // throw an exception if the denominator is not 1
+        throw std::invalid_argument("Denominator must be 1 to use pre-decrement operator.");
+    }
+}
+
+Rational Rational::operator++(int) {
+    // post-increment operator
+    if (denominator == 1) {
+        Rational temp(*this);
+        numerator++;
+        return temp;
+    }
+    else {
+        // throw an exception if the denominator is not 1
+        throw std::invalid_argument("Denominator must be 1 to use post-increment operator.");
+    }
+}
+
+Rational Rational::operator--(int) {
+    // post-decrement operator
+    if (denominator == 1) {
+        Rational temp(*this);
+        numerator--;
+        return temp;
+    }
+    else {
+        // throw an exception if the denominator is not 1
+        throw std::invalid_argument("Denominator must be 1 to use post-decrement operator.");
+    }
+}
+
+double Rational::getHighestCommonFactor(double a, double b) const {
+    // Euclidean algorithm to find the getHighestCommonFactor of a and b
+    while (b != 0) {
+	    const double auxiliar = b;
+        b = fmod(a, b);
+        a = auxiliar;
+    }
+    return a;
 }
--- Lab5BB/Rational.h
+++ Lab5BB/Rational.h
@@ -23,12 +23,19 @@ public:
     friend std::istream& operator>>(std::istream& in, Rational& r);
     friend std::ostream& operator<<(std::ostream& out, const Rational& r);
 
+    // Overloaded relational and equality operators
     bool operator<(const Rational& other) const;
     bool operator>(const Rational& other) const;
     bool operator<=(const Rational& other) const;
     bool operator>=(const Rational& other) const;
     bool operator!=(const Rational& other) const;
     bool operator==(const Rational& other) const;
+
+    // Overloaded increment and decrement operators
+	Rational & operator++();         // pre-increment
+    Rational& operator--();         // pre-decrement
+    Rational operator++(int);       // post-increment
+    Rational operator--(int);       // post-decrement
 
     double getCurrentValue() const;
 
