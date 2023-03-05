ade64817720c9dae9beb5819f1f86d5b87eb78ed
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-03-05 16:47:40 -0500

    added lab 6

--- Lab6BB/Lab6BB.cpp
+++ Lab6BB/Lab6BB.cpp
@@ -19,18 +19,13 @@ struct Coffee
     std::string flavorProfile;
     double pricePerKG{};
 };
-std::list<std::string> listOfSpecificFlavors ={ "Creamy", "Full Bodied"};
 
-template<typename Func>
-bool isIt(std::string flavour, Func func)
+void printEnum(std::vector<Coffee> stock)
 {
-    return func(flavour);
-}
-
-template<typename Func>
-bool isCondition(Coffee coffee, Func func)
-{
-    return func(coffee);
+    for (Coffee coffee : stock)
+    {
+        std::cout << static_cast<std::underlying_type<roastType>::type>(coffee.roast) << std::endl;
+    }
 }
 
 bool isCreamyOrFullFlavour(const Coffee coffee)
@@ -41,14 +36,6 @@ bool isCreamyOrFullFlavour(const Coffee coffee)
 bool isLightOrMediumAndPriceLess10(const Coffee coffee)
 {
     return (coffee.roast == roastType::light || coffee.roast == roastType::medium) && coffee.pricePerKG < 10.0;
-}
-
-void printEnum(std::vector<Coffee> stock)
-{
-    for (Coffee coffee : stock)
-    {
-        std::cout << static_cast<std::underlying_type<roastType>::type>(coffee.roast) << std::endl;
-    }
 }
 
 bool isRoast(const Coffee coffee)
@@ -69,64 +56,34 @@ double getTotalStockPrice(std::list<Coffee> coffeeSubs, const long subsSize)
 std::list<Coffee> createSubscription(std::vector<Coffee> stock)
 {
     std::list<Coffee> coffeeSubsList;
-	std::cout << "Number of Coffees with 'Creamy' or 'Full Bodied' flavor profile: "
-        << std::count_if(stock.begin(), stock.end(), isCreamyOrFullFlavour) << "\n";
+
+	/*std::cout << "Number of Coffees with 'Creamy' or 'Full Bodied' flavor profile: "
+        << std::count_if(stock.begin(), stock.end(), isCreamyOrFullFlavour) << "\n";*/
+    std::cout << "Number of Coffees with 'Creamy' or 'Full Bodied' flavor profile: "
+        << std::count_if(stock.begin(), stock.end(), [](const Coffee& coffee) {
+			return coffee.flavorProfile == "Creamy" || coffee.flavorProfile == "Full Bodied";
+            }) << "\n";
+
     std::sort(stock.begin(), stock.end(), [](const Coffee& c1, const Coffee& c2) {
         return c1.roast < c2.roast;
         });
-    std::copy_if(stock.begin(), stock.end(), std::back_inserter(coffeeSubsList), isLightOrMediumAndPriceLess10);
-
+    //std::copy_if(stock.begin(), stock.end(), std::back_inserter(coffeeSubsList), isLightOrMediumAndPriceLess10);
+    std::copy_if(stock.begin(), stock.end(), std::back_inserter(coffeeSubsList), [](const Coffee& coffee) {
+        return (coffee.roast == roastType::light || coffee.roast == roastType::medium) && coffee.pricePerKG < 10.0;
+    });
 	const auto darkRoastIt = std::find_if(stock.begin(), stock.end(), isRoast);
     if (darkRoastIt != stock.end()) {
         coffeeSubsList.push_back(*darkRoastIt);
-    }
-	
-    const double total = getTotalStockPrice(coffeeSubsList, coffeeSubsList.size());
-    std::cout << "Total price per KG of Coffees in the subscription: $" << total << "\n";
+    }	
+    //const double total = getTotalStockPrice(coffeeSubsList, coffeeSubsList.size());
+    const double total = std::accumulate(coffeeSubsList.begin(), coffeeSubsList.end(), 0.0, [](const double tot_sum, const Coffee& coffee) {
+        return tot_sum + coffee.pricePerKG;
+        });
+    std::cout << "Total price per KG of Coffees in the subscription list is: $" << total << "\n";
     return coffeeSubsList;
 }
 
 
-
-
-
-/*std::list<Coffee> createSubscription(const std::vector<Coffee>& stock)
-{
-    // Step 1: Print the number of coffees with "Creamy" or "Full Bodied" flavor profile
-    std::cout << "Number of Coffees with 'Creamy' or 'Full Bodied' flavor profile: "
-        << std::count_if(stock.begin(), stock.end(), [](const Coffee& coffee) {
-        return coffee.flavorProfile == "Creamy" || coffee.flavorProfile == "Full Bodied";
-            }) << "\n";
-
-    // Step 2: Sort the stock based on roastType (in the order of light, medium, dark)
-    std::sort(stock.begin(), stock.end(), [](const Coffee& c1, const Coffee& c2) {
-        return c1.roast < c2.roast;
-        });
-
-    // Step 3: Create a list of Coffees that contains only light and medium roasts
-    // with price less than $10 per KG
-    std::list<Coffee> subscription;
-    std::copy_if(stock.begin(), stock.end(), std::back_inserter(subscription), [](const Coffee& coffee) {
-        return (coffee.roast == roastType::light || coffee.roast == roastType::medium) && coffee.pricePerKG < 10.0;
-        });
-
-    // Step 4: Add the first dark roast coffee to the subscription
-    auto darkRoastIt = std::find_if(stock.begin(), stock.end(), [](const Coffee& coffee) {
-        return coffee.roast == roastType::dark;
-        });
-    if (darkRoastIt != stock.end()) {
-        subscription.push_back(*darkRoastIt);
-    }
-
-    // Step 5: Tally up the total price per KG of all the Coffees in the subscription and print it to the screen
-    double total = std::accumulate(subscription.begin(), subscription.end(), 0.0, [](double sum, const Coffee& coffee) {
-        return sum + coffee.pricePerKG;
-        });
-    std::cout << "Total price per KG of Coffees in the subscription: $" << total << "\n";
-
-    return subscription;
-}
-*/
 
 int main()
 {
